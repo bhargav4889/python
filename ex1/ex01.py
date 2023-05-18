af7e9f13c11a341f8b7aac69143155b6f596e82a
BHARGAV <54997063+bhargav4889@users.noreply.github.com>
2023-05-18 13:24:02 +0530

    Update ex01.py

--- ex1/ex01.py
+++ ex1/ex01.py
@@ -1,120 +1,94 @@
-# Create one class named "category" with members "name", "code", "no_of_products"
+#  Create one class named "category" with members "name", "code", "no_of_products"
 
 class Category:
-    def __init__(self, name, no_of_products):
+    def __init__(self, name, code, no_of_products):
         self.name = name
+        self.code = code
         self.no_of_products = no_of_products
 
-    def display_all(self):
-        print('\n', 'CategoryName: ', self.name, '\n', 'No_Of_Products: ', self.no_of_products)
+    def display(self):
+        print("Category Name: ", self.name, '\n'"Code is: ", self.code, '\n''No Of Products: ', self.no_of_products)
+
+    def __str__(self):
+        return self.name
 
 
-c = Category()
-c.display_all()
+c1 = Category(name='Mobile', code=12, no_of_products=200)
 
 
-# --------------------------------------#
+# c1.display()
+
 
 # Create one class named "product" with member "name", "code", "category", "Price"
 
 class Product:
-    def __init__(self, name, category, price):
+    def __init__(self, name, code, category, price):
         self.name = name
+        self.code = code
         self.category = category
         self.price = price
 
-    def displayproductdetails(self):
-        print('\n', 'Product Name: ', self.name, '\n', 'Category: ', self.category, '\n', 'Product Price: ', self.price)
+    def info(self):
+        print("Product name: ", self.name, '\n''Product Code: ', self.code, '\n''Category is: ', self.category,
+              '\n''Price is: ', self.price)
+
+    def __str__(self):
+        return f'{self.price}'
 
 
-#
-#
-pr = Product()
-pr.displayproductdetails()
-
-# ----------------------------------------------#
+iphone = Product(name='iphone-12', code=1, category=c1, price=21000)
+# iphone.info()
 
 # Create three objects of a category.
+vegetable = Category(name='Vegetable', no_of_products=200, code=20)
+bikes = Category(name="Bikes", no_of_products=100, code=21)
+fruits = Category(name="Fruits", no_of_products=123, code=12)
 
-cat1 = Category(name='Mobile', no_of_products=100)
-cat2 = Category(name='Bike', no_of_products=200)
-cat3 = Category(name='Car', no_of_products=300)
-cat1.display_all()
-cat2.display_all()
-cat3.display_all()
+# Create 10 different products.The code must be unique.
 
-# ----------------------------------------------#
-
-# Create 10 different products. The code must be unique.
-
-product1 = Product(name='iPhone-5', category='Mobile', price=199)
-product2 = Product(name='iPhone-6', category='Mobile', price=299)
-product3 = Product(name='iPhone-6s', category='Mobile', price=399)
-product4 = Product(name='iPhone-7', category='Mobile', price=499)
-product5 = Product(name='iPhone-8+', category='Mobile', price=599)
-product6 = Product(name='iPhone-X', category='Mobile', price=699)
-product7 = Product(name='iPhone-XR', category='Mobile', price=799)
-product8 = Product(name='iPhone-11', category='Mobile', price=899)
-product9 = Product(name='iPhone-12', category='Mobile', price=999)
-product10 = Product(name='iPhone-13', category='Mobile', price=1199)
-
-list1 = [product1, product2, product3, product4, product5, product6, product7, product8, product9, product10]
-
-for i in list1:
-    print('\n', 'Product Name: ', i.name, '\n', 'Category: ', i.category, '\n', 'Product-Price: ', i.price)
-
-# ----------------------------------------------#
+Corns = Product(name='Corns', code=1, category=vegetable, price=200)
+Onions = Product(name='Onion', code=2, category=vegetable, price=100)
+Bananas = Product(name='banana', code=3, category=fruits, price=100)
+HondaDio = Product(name="Honda-Dio", code=3, category=bikes, price=399999)
+HondaShine = Product(name='Honda-Shine', code=4, category=bikes, price=499999)
+Potatos = Product(name="Potato", code=5, category=vegetable, price=200)
+Peas = Product(name='Peas', code=7, category=vegetable, price=100)
+Watermelon = Product(name='WaterMelon', code=8, category=fruits, price=300)
+Orange = Product(name='Orange', code=9, category=fruits, price=124)
+Mango = Product(name='Mango', code=10, category=fruits, price=200)
 
 # Print category info with its no_of_products
-category_info = {
-    'Mobile': 100,
-    'Car': 200,
-    'Bike': 300
-}
-for i, j in category_info.items():
-    print('\n', 'Category-Name is: ', i, '\n', 'No_Of_Products: ', j)
+list_of_categories = [vegetable, bikes, fruits]
 
-# dict methods
-# dict.keys()=> keys print
-# dict.values()=> values only print
-# dict.items()=>keys&values are print
+# for i in list_of_categories:
+#     print("Category Name: ", i.name, '\n''No_Of_Product: ', i.no_of_products)
 
-# ----------------------------------------------#
-# Sort and Print products based on price ( Price High to Low and Low to High) with all details.
-# use bubble sort
+# Sort and Print products based on price ( Price High to Low and Low to High) with all details
+list_of_products = [Corns, Onions, Bananas, HondaDio, HondaShine, Potatos, Peas, Watermelon, Orange, Mango]
 
-products = {'Mobile': 1000, 'Bottle': 500, 'Fruits': 900}
-productslist = list(products.items())
-print(productslist)
+length = len(list_of_products)
+#
+# # price low to high
+# for i in range(length - 1):
+#     for j in range(i + 1, length):
+#         if list_of_products[i] > list_of_products[j]:
+#             temp = list_of_products[i]
+#             list_of_products[i] = list_of_products[j]
+#             list_of_products[j] = temp
+# print('Product_Price_Low_To_High', '\n', list_of_products)
 
-length = len(productslist)
-print(length)
-
-# price low to high
-for i in range(length - 1):
-    for j in range(i + 1, length):
-        if productslist[i] > productslist[j]:
-            temp = productslist[i]
-            productslist[i] = productslist[j]
-            productslist[j] = temp
-print('Product_Price_Low_To_High', '\n', productslist)
-
-# price high to low
-for i in range(length - 1):
-    for j in range(i + 1, length):
-        if productslist[i] < productslist[j]:
-            temp = productslist[i]
-            productslist[i] = productslist[j]
-            productslist[j] = temp
-print('Product_Price_High_To_Low', '\n', productslist)
-
-# ----------------------------------------------#
+# # price high to low
+# for i in range(length - 1):
+#     for j in range(i + 1, length):
+#         if list_of_products[i] < list_of_products[j]:
+#             temp = list_of_products[i]
+#             list_of_products[i] = list_of_products[j]
+#             list_of_products[j] = temp
+# print('Product_Price_High_To_Low', '\n', list_of_products)
 
 # Search product using its code.
-# use in & not-in operator
-products = ['Mobile', 'Car', 'Bike', 'Bottle', 'Glasses', 'T-shirt']
-search = input('Enter Word To Search: ')
-if search in products:
-    print('Word is Found in List: ', search)
-else:
-    print('Word is Not Found in List: ', search)
+
+code = int(input("Enter Code: "))
+for i in list_of_products:
+    if code == i.code:
+        print("Product Name is: ", i.name, '\n'"Product Code: ", i.code)
