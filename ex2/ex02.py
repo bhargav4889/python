10f999ed7b8abd309fca27a5e2c6ab9a262177ff
BHARGAV <54997063+bhargav4889@users.noreply.github.com>
2023-05-23 16:41:59 +0530

    Update ex02.py

--- ex2/ex02.py
+++ ex2/ex02.py
@@ -1,194 +1,129 @@
-# Add new data members “parent”, “display_name”, and “products” (list of product objects) inside the category class.
+from ex1.ex01 import Product as PR
+
+
+# Add new data members “parent”, “display_name”, and “products” (list of product objects) inside the category class
+
 class Category:
-    def __init__(self, parent='parent', display_name='display', products=[], productname=None, productprice=None):
+    def __init__(self, display_name, parent=None):
         self.parent = parent
         self.display_name = display_name
-        self.products = products
-        self.productname = productname
-        self.productprice = productprice
-        products.append(productname)
-        products.append(productprice)
-        print(products)
-
-
-p1 = Category(productname='iphone', productprice=100)
-p2 = Category(productprice=100, productname='watch')
-p3 = Category(productprice=200, productname='bike')
-p4 = Category(productprice=21, productname='toys')
-
-list1 = [p1, p2, p3, p4]
-
-for i in list1:
-    print('\n', 'productname: ', i.productname, '\n', 'productprice: ', i.productprice)
-print(p1.parent)
-print(p1.display_name)
-
-
-# ----------------------------------------------#
-
-# Add a new member function to generate “display_name”
-
-class Demo:
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-
-    def display_name(self):
-        print('\n''Your Name: ', self.name, '\n''Your Email: ', self.email)
-
-
-d = Demo(name='bhargav', email='bhargavkachhhela1@gmail.com')
-d.display_name()
-
-
-# ----------------------------------------------#
-
-# “display_name” has a text value as below.
-# Vehicle category without parent then “Vehicle”
-# Car category with “Vehicle” as a parent then “Vehicle > Car”
-# Petrol category with “Car” as a parent then “Vehicle > Car Petrol”
-#
-class Vehicle:
-    def __init__(self, vehicle):
-        self.vehicle = vehicle
-        print(self.vehicle)
-
-    class Car:
-        def __init__(self, name, parent=None):
-            self.name = name
-            self.parent = parent
-
-        def display_name(self):
-            print('Vehicle >', self.parent, )
-
-    class Fuel:
-        def __init__(self, fueltype, parent=None):
-            self.fueltype = fueltype
-            self.parent = parent
-
-        def display_name(self):
-            print('Car >', self.parent)
-
-
-v = Vehicle('Vehicle')
-v.Car('HondaCity', parent=v.Car.__name__).display_name()
-v.Fuel('Petrol', parent=v.Fuel.__name__).display_name()
-
-
-# ----------------------------------------------#
-
-# Create 5 category objects with parent and child relation.
-class Category:
-    def __init__(self, name, parent=None):
-        self.name = name
-        self.parent = parent
-        self.child = []
-        if self.parent is not None:
-            self.child.append(self.parent)
-            # print(self.child)
+        self.products = []
 
     def display(self):
-        print(self.name, '\n')
-        # print(self.parent)
-        # print(self.child)
+        if self.parent is None:
+            print(self.parent)
+        else:
+            print(self.parent.display_name, '>', self.display_name)
+
+    def __str__(self):
+        return f'{self.parent}'
 
 
-# parent
-electronics = Category("Electronics")
-food = Category("Food")
-watch = Category("Watch")
+Vehicle = Category(display_name="Vehicle")
+Car = Category(parent=Vehicle, display_name="Car")
+Bike = Category(parent=Vehicle, display_name="Bike")
+Petrol = Category(parent=Bike, display_name="Petrol")
+Diesel = Category(parent=Car, display_name='Diesel')
+
+Vehicle.display()
+Bike.display()
+Car.display()
+Petrol.display()
+Diesel.display()
+
+# Vehicle is Parent Class It Has Two Child Class --> 1)Bike 2)Car
+# Bike Class Has One Child Class --> Petrol
+# Car Class has One Child Class --> Diesel
+
+Mustang = PR(name="Mustang", category=Vehicle.display_name, price=190, code=10)
+RollsRoyce = PR(name='Rolls-Royce', category=Vehicle.display_name, price=200, code=11)
+BMW = PR(name='BMW', category=Vehicle.display_name, price=240, code=12)
+
+Vehicle.products.insert(0, Mustang)
+Vehicle.products.insert(1, RollsRoyce)
+Vehicle.products.insert(2, BMW)
+
+Swift = PR(name="Swift-Turbo", category=Car.display_name, price=1999, code=21)
+HondaCity = PR(name='Honda-City-Elite', category=Car.display_name, price=344, code=22)
+Toyota = PR(name='Toyota-Corolla', category=Car.display_name, price=657, code=23)
+
+Car.products.append(Swift)
+Car.products.append(HondaCity)
+Car.products.append(Toyota)
+
+# for i in Car.products:
+#     print(i.name)
+
+RoadBikes = PR(name="AeroStorm", category=Bike.display_name, price=15999, code=31)
+ElectricBikes = PR(name="TurboBoost", category=Bike.display_name, price=16999, code=32)
+CityBikes = PR(name="CitySprint", category=Bike.display_name, price=20000, code=33)
+
+Bike.products.append(RoadBikes)
+Bike.products.append(ElectricBikes)
+Bike.products.append(CityBikes)
+
+# for i in Bike.products:
+#     print(i.name)
+
+Power_Generation = PR(name="AdaniGreenEnergy", category=Petrol.display_name, price=900, code=41)
+Small_Engines = PR(name='PetrolEngines', category=Petrol.display_name, price=20000, code=42)
+Small_machinery = PR(name='PilarPressMachine', category=Petrol.display_name, price=25000, code=43)
+
+Petrol.products.append(Power_Generation)
+Petrol.products.append(Small_Engines)
+Petrol.products.append(Small_machinery)
 #
-# child
-mobile = Category("Mobile", parent=electronics).display()
-laptop = Category("Laptop", parent=electronics).display()
-fruits = Category("Fruits", parent=food).display()
-iwatch = Category("I-Watch", parent=watch).display()
-tomato = Category('Tomato', parent=food).display()
+# for i in Petrol.products:
+#     print(i.name)
 
-# ----------------------------------------------#
+Trains = PR(name='Train-Engines', category=Diesel.display_name, price=50000, code=51)
+Generator = PR(name='Generator-Machine', category=Diesel.display_name, price=258000, code=52)
+Rocket = PR(name="Spaceship", category=Diesel.display_name, price=450000, code=53)
 
-# Create 3 product objects in each category.
-
-iphone = Category('iPhone-12', parent=mobile).display()
-hp = Category('Hp Laptop', parent=laptop).display()
-iwatch7 = Category('iWatch7', parent=iwatch).display()
+Diesel.products.append(Trains)
+Diesel.products.append(Generator)
+Diesel.products.append(Rocket)
 
 
-# ----------------------------------------------#
-
-# Display the Category with its Code, Display Name, and all product details inside that category.
-
-class Category:
-    def __init__(self, code, displayname, productid, productname, productprice, productinfo):
-        self.code = code
-        self.displayname = displayname
-        self.productid = productid
-        self.productname = productname
-        self.productprice = productprice
-        self.productinfo = productinfo
+def display(Object):
+    for i in Object.products:
+        # print(i.category)
+        print("Product Name: ", i.name, '\n''Product Code: ', i.code, '\n''Category: ', i.category, '\n'"Price: ",
+              i.price, '\n')
 
 
-class FoodProduct(Category):
-    def __init__(self, code, displayname, productid, productname, productprice, productinfo):
-        super().__init__(code, displayname, productid, productname, productprice, productinfo)
+display(Object=Car)
+display(Object=Bike)
+display(Object=Vehicle)
+display(Object=Petrol)
+display(Object=Diesel)
 
-    def display(self):
-        print('\n', 'Category-Code: ', self.code, '\n', 'Display-Name: ', self.displayname, '\n', 'Product-Id: ',
-              self.productid,
-              '\n', 'Product-Name: ', self.productname, '\n', 'Product-Price: ', self.productprice, '\n',
-              'Product-Info: ', self.productinfo)
-
-
-class DeviceProduct(Category):
-    def __init__(self, code, displayname, productid, productname, productprice, productinfo):
-        super().__init__(code, displayname, productid, productname, productprice, productinfo)
-
-    def display(self):
-        print('\n', 'Category-Code: ', self.code, '\n', 'Display-Name: ', self.displayname, '\n', 'Product-Id: ',
-              self.productid,
-              '\n', 'Product-Name: ', self.productname, '\n', 'Product-Price: ', self.productprice, '\n',
-              'Product-Info: ',
-              self.productinfo)
+list_of_products = [Mustang, BMW, RollsRoyce, Swift, Toyota, HondaCity, RoadBikes, ElectricBikes, CityBikes,
+                    Power_Generation, Small_Engines, Small_machinery, Trains, Generator, Rocket]
+list_of_categories = [Car, Diesel, Petrol, Vehicle, Bike]
 
 
-# food class
-burger = FoodProduct(code=19, displayname='Fast- Food', productid=11, productname='Burger', productprice=100,
-                     productinfo='Chezze Buger', ).display()
-pizza = FoodProduct(code=29, displayname='Fast-Food', productid=2, productname='Pizza', productprice=299,
-                    productinfo='italian Pizza').display()
+def order_by_category(list1):
+    length = len(list1)
+    for i in range(0, length - 1):
+        for j in range(length - i - 1):
+            if list1[j].category > list1[j + 1].category:
+                list1[j], list1[j + 1] = list1[j + 1], list1[j]
+    return list1
 
-# device class
-iphone = DeviceProduct(code=21, displayname='mobile', productid=2129, productname='iphone-12', productprice=48888,
-                       productinfo='Apple iphone12').display()
-hp = DeviceProduct(code=18, displayname='Laptop', productid=2122, productname='Hp Pro', productprice=29999,
-                   productinfo='Hp Gaming Laptop').display()
 
-# ----------------------------------------------#
+order_by_category(list1=list_of_products)
+for i in list_of_products:
+    print("Product Name: ", i.name, '\n''Product Code: ', i.code, '\n''Category: ', i.category, '\n'"Price: ",
+          i.price, '\n')
 
-# Display product list by category ( group by category, order by category name).
-#
-products = [
-    {'name': 'Bike', 'category': 'Vehicle'},
-    {'name': 'Burger', 'category': 'Food'},
-    {'name': 'Laptop', 'category': 'Device'},
-    {'name': 'Mobile', 'category': 'Device'},
-]
 
-# empty dict
-products_by_category = {}
+def group_by_category(list2):
+    for i in list2:
+        for j in i.products:
+            print("Product Name: ", j.name, '\n''Product Code: ', j.code, '\n''Category: ', j.category, '\n'"Price: ",
+                  j.price, '\n')
+        print('\n')
 
-for product in products:
-    category = product["category"]
-    if category not in products_by_category:
-        products_by_category[category] = []
-    products_by_category[category].append(product)
 
-categories = list(products_by_category.keys())
-
-categories.sort()
-
-for category in categories:
-    print(f"{category}:")
-    category_products = products_by_category[category]
-    for product in category_products:
-        print(f"- {product['name']}")
+group_by_category(list_of_categories)
