47491a540222d8f4787b07bed3e53116d1321c2d
BHARGAV <54997063+bhargav4889@users.noreply.github.com>
2023-05-25 15:31:39 +0530

    Update ex02.py

--- ex2/ex02.py
+++ ex2/ex02.py
@@ -3,7 +3,6 @@ from ex1.ex01 import Product as PR
 
 # Add new data members “parent”, “display_name”, and “products” (list of product objects) inside the category class
 
-
 class Category:
     def __init__(self, display_name, parent=None):
         self.display_name = display_name
@@ -16,6 +15,26 @@ class Category:
         else:
             return f'{self.parent.display_info()} > {self.display_name}'
 
+    def __str__(self):
+        return f'{self.products}'
+
+    def order_by_category(self, list1):
+        length = len(list1)
+        for i in range(0, length - 1):
+            for j in range(length - i - 1):
+                if list1[j].category > list1[j + 1].category:
+                    list1[j], list1[j + 1] = list1[j + 1], list1[j]
+        for k in list1:
+            print("Category name: ", k.category, '\n''Product Name: ', k.name)
+
+    def group_by_category(self, list2):
+        for i in list2:
+            for j in i.products:
+                print("Product Name: ", j.name, '\n''Product Code: ', j.code, '\n''Category: ', j.category,
+                      '\n'"Price: ",
+                      j.price, '\n')
+            print('\n')
+
 
 Vehicle = Category(display_name="Vehicle")
 Car = Category(display_name="Car", parent=Vehicle)
@@ -23,7 +42,7 @@ Bike = Category(display_name="Bike", parent=Vehicle)
 Petrol = Category(parent=Bike, display_name="Petrol")
 Diesel = Category(parent=Car, display_name='Diesel')
 NewObj = Category(parent=Diesel, display_name="newobj2")
-NewObj2 = Category(parent=NewObj, display_name='Neww')
+NewObj2 = Category(parent=NewObj, display_name='New')
 
 print(Vehicle.display_info())
 print(Bike.display_info())
@@ -53,9 +72,6 @@ Car.products.append(Swift)
 Car.products.append(HondaCity)
 Car.products.append(Toyota)
 
-# for i in Car.products:
-#     print(i.name)
-
 RoadBikes = PR(name="AeroStorm", category=Bike.display_name, price=15999, code=31)
 ElectricBikes = PR(name="TurboBoost", category=Bike.display_name, price=16999, code=32)
 CityBikes = PR(name="CitySprint", category=Bike.display_name, price=20000, code=33)
@@ -64,9 +80,6 @@ Bike.products.append(RoadBikes)
 Bike.products.append(ElectricBikes)
 Bike.products.append(CityBikes)
 
-# for i in Bike.products:
-#     print(i.name)
-
 Power_Generation = PR(name="AdaniGreenEnergy", category=Petrol.display_name, price=900, code=41)
 Small_Engines = PR(name='PetrolEngines', category=Petrol.display_name, price=20000, code=42)
 Small_machinery = PR(name='PilarPressMachine', category=Petrol.display_name, price=25000, code=43)
@@ -74,9 +87,6 @@ Small_machinery = PR(name='PilarPressMachine', category=Petrol.display_name, pri
 Petrol.products.append(Power_Generation)
 Petrol.products.append(Small_Engines)
 Petrol.products.append(Small_machinery)
-#
-# for i in Petrol.products:
-#     print(i.name)
 
 Trains = PR(name='Train-Engines', category=Diesel.display_name, price=50000, code=51)
 Generator = PR(name='Generator-Machine', category=Diesel.display_name, price=258000, code=52)
@@ -87,45 +97,37 @@ Diesel.products.append(Generator)
 Diesel.products.append(Rocket)
 
 
-def display(Object):
-    for i in Object.products:
-        # print(i.category)
-        print("Product Name: ", i.name, '\n''Product Code: ', i.code, '\n''Category: ', i.category, '\n'"Price: ",
-              i.price, '\n')
-
-
-display(Object=Car)
-display(Object=Bike)
-display(Object=Vehicle)
-display(Object=Petrol)
-display(Object=Diesel)
-
 list_of_products = [Mustang, BMW, RollsRoyce, Swift, Toyota, HondaCity, RoadBikes, ElectricBikes, CityBikes,
                     Power_Generation, Small_Engines, Small_machinery, Trains, Generator, Rocket]
 list_of_categories = [Car, Diesel, Petrol, Vehicle, Bike]
 
+#
+new = Category(display_name='')
+new.order_by_category(list_of_products)
+new.group_by_category(list_of_categories)
 
-def order_by_category(list1):
-    length = len(list1)
-    for i in range(0, length - 1):
-        for j in range(length - i - 1):
-            if list1[j].category > list1[j + 1].category:
-                list1[j], list1[j + 1] = list1[j + 1], list1[j]
-    return list1
-
-
-order_by_category(list1=list_of_products)
-for i in list_of_products:
-    print("Product Name: ", i.name, '\n''Product Code: ', i.code, '\n''Category: ', i.category, '\n'"Price: ",
-          i.price, '\n')
-
-
-def group_by_category(list2):
-    for i in list2:
-        for j in i.products:
-            print("Product Name: ", j.name, '\n''Product Code: ', j.code, '\n''Category: ', j.category, '\n'"Price: ",
-                  j.price, '\n')
-        print('\n')
+# ----------------------------------------------------------------------------#
+# def order_by_category(list1):
+#     length = len(list1)
+#     for i in range(0, length - 1):
+#         for j in range(length - i - 1):
+#             if list1[j].category > list1[j + 1].category:
+#                 list1[j], list1[j + 1] = list1[j + 1], list1[j]
+#     return list1
 
 
-group_by_category(list_of_categories)
+# order_by_category(list1=list_of_products)
+# for i in list_of_products:
+#     print("Product Name: ", i.name, '\n''Product Code: ', i.code, '\n''Category: ', i.category, '\n'"Price: ",
+#           i.price, '\n')
+#
+#
+# def group_by_category(list2):
+#     for i in list2:
+#         for j in i.products:
+#             print("Product Name: ", j.name, '\n''Product Code: ', j.code, '\n''Category: ', j.category, '\n'"Price: ",
+#                   j.price, '\n')
+#         print('\n')
+
+#
+# group_by_category(list_of_categories)
